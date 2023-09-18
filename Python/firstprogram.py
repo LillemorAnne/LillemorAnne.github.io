81f5965cfa70db91489b3c3ea98fe596a9d74790
Anne-Lina <annelinakn@gmail.com>
2023-09-18 19:32:06 +0200

    Tilføjelse af svar og aar

--- Python/firstprogram.py
+++ Python/firstprogram.py
@@ -1,23 +1,13 @@
 # This program says hello and asks for my name.
-print('Hello world!.')
+print('Hello my dear!.')
+myAnswer = input()
 print('What is your name?')# ask for their name
 myName = input() 
 print('It is good to meet you, ' +myName) 
 print('The length of your name is:')
 print(len(myName))
-
-from datetime import datetime
-
-datetime.now()
-print('What is you birthday in numbers?')
-myBirthday = input()
-
-difference = (myBirthday - datetime)
-
-print(str(difference) + " days until your birthday")
-print(str(difference.total_seconds()/60) + " minutes until your birthday")
-print(str(difference.total_seconds()) + " seconds until your birthday")
-
+print('How old are you?')
+myAge = input()
 print('Which country do you live in?') #ask for which country they live in
 myCountry = input()
 print(myCountry+' is a beautiful country')
