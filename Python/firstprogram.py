721bb9d83a16646f7253d71331fcd3da89b74b66
Anne-Lina <annelinakn@gmail.com>
2023-09-18 20:13:18 +0200

    note+

--- Python/firstprogram.py
+++ Python/firstprogram.py
@@ -5,7 +5,7 @@ print('What is your name?')# ask for their name
 print ('My name is:')
 myName = input()
 print('It is good to meet you, ' + (myName)) 
-print('How old are you?')
+print('How old are you?')#ask about age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' on your next birthday')
 print('Which country do you live in?') #ask for which country they live in
