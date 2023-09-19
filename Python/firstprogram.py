660e3b8957e7910f5853e9139aaab572284f8fa3
Anne-Lina <annelinakn@gmail.com>
2023-09-19 11:06:18 +0200

    ændret tekst

--- Python/firstprogram.py
+++ Python/firstprogram.py
@@ -2,15 +2,18 @@
 print('Hello my dear!.')
 myAnswer = input()
 print('What is your name?')# ask for their name
-print ('My name is:')
+print ('My name is:') #the answer
 myName = input()
 print('It is good to meet you, ' + (myName)) 
 print('How old are you?')#ask about age
 myAge = input()
+print('You are old! :-) ')
 print('You will be ' + str(int(myAge) + 1) + ' on your next birthday')
+print('..........')
 print('Which country do you live in?') #ask for which country they live in
 myCountry = input()
 print(myCountry+' is a beautiful country')
 print('Which city do you live in?') #ask which country they live 
 myCity = input()
-print(myCity+ ' is a nice city')
+print('didnt now about ' +myCity )
+
