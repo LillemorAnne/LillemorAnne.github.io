f71e96ce4eb4352f0ad4d3bbb48f30f745770cd1
Anne-Lina <annelinakn@gmail.com>
2023-11-28 09:54:50 +0100

    Tilføjet flere muligheder

--- Selv og mine opgaver/Kap1.py
+++ Selv og mine opgaver/Kap1.py
@@ -4,6 +4,8 @@ print('Hvad er dit navn?')
 myName = input()
 if name == 'Lillemor':
     print('Hej Anne')
+else: 
+    print('Husk denne computer tilhøre Anne, som er lille og passer på alle som en mor.')
     myPassword = input()
     print('Hvilken er din adgangskode?')
     if password == 'Tøsen':
