b881b4e3eb283273bc934dd6da1cb3688b26abe0
Anne-Lina <annelinakn@gmail.com>
2023-12-11 09:00:17 +0100

    oprettelse af day2 og filer til

--- Selv og mine opgaver/Virker ikke.py
+++ Selv og mine opgaver/Virker ikke.py
@@ -1,10 +1,10 @@
 while True:
     print('hvem er du?')
     name = input
-    if name != 'Anne':
+    if name == 'Anne':
         continue
     print('Hej, Anne. Hvad er din kode?(Din hund+år du fik hende)')
     password = input()
     if password == 'Tøsen2022':
-        break
-    print('Velkommen.')
+         break
+print('Velkommen.')
